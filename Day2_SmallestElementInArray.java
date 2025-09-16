1d62a34a1839ec8cb5c5f4cb0adf3ea1d5ddf2b2
Firoz Hussain <hfiroz86@gmail.com.com>
2025-09-16 23:44:34 +0530

    smallest largest

--- Day2_SmallestElementInArray.java
+++ Day2_SmallestElementInArray.java
@@ -3,27 +3,35 @@ public class Day2_SmallestElementInArray {
 
     // Function to find smallest element
     public static int findSmallest(int[] arr) {
-        if (arr == null || arr.length == 0) {
-            throw new IllegalArgumentException("Array is empty!");
+        if(arr == null || arr.length == 0){
+            return 0;
         }
-
-        int min = arr[0]; // assume first element is smallest
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] < min) {
-                min = arr[i];
-            }
-        }
-        return min;
+    
+        if(arr.length == 1)
+         return arr[0];
+    
+    int n = arr.length;
+    int mini =arr[0]; //3
+     for(int i = 1;i<n;i++){ //  5 2 9 1
+    if(arr[i]<mini){
+        mini = arr[i];
     }
+      
+     }
+     return mini;
+    }
+
 
     public static void main(String[] args) {
         int[] arr1 = {3, 5, 2, 9, 1};
         int[] arr2 = {-5, -2, -10};
         int[] arr3 = {42};
+        int[] arr4 = {};
 
         System.out.println("Smallest in arr1: " + findSmallest(arr1)); // 1
         System.out.println("Smallest in arr2: " + findSmallest(arr2)); // -10
         System.out.println("Smallest in arr3: " + findSmallest(arr3)); // 42
+        System.out.println("Smallest in arr3: " + findSmallest(arr4)); 
     }
 }
 
